287ad5a9eac66f594142af9db5949a9a59812890
gabrielarueda <sugey11.rueda@gmail.com>
2023-02-28 20:30:06 -0400

    actu skeleton

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const configs = {
     api: {
-        port: process.env.PORT || 3000,
-        host: process.env.HOST || 'http://localhost:3000',
-        nodeEnv: process.env.NODE_ENV || 'development'
+        port: process.env.PORT || 9000,
+        host: process.env.HOST || 'http://localhost:9000',
+        nodeEnv: process.env.NODE_ENV || 'development',
+        secretOrKey: process.env.JWT_SECRET
     },
     db: {
         development: {
@@ -20,13 +22,14 @@ const configs = {
             }
         },
         production: {
+             // aqui van los datos reales por lo que aqui deben ir variables de 
             //? Aqui deberan estar las configuraciones para la conexion con sequelize
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'root',
-            database: 'chat-db',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             define: {
                 timestamps: true, //? Nos obliga a que todas las tablas tengan la propiedad createdAt y upadtedAt
                 underscored: true,
